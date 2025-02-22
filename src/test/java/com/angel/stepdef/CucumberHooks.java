64345ba1f0cc7ddea8512a169c77e4ff504da475
Angel <angelaindahpuspita@gmail.com>
2025-02-22 11:11:29 +0800

    revis

--- src/test/java/com/angel/stepdef/CucumberHooks.java
+++ src/test/java/com/angel/stepdef/CucumberHooks.java
@@ -3,16 +3,21 @@ package com.angel.stepdef;
 import com.angel.BaseTest;
 import io.cucumber.java.After;
 import io.cucumber.java.Before;
-import io.github.bonigarcia.wdm.WebDriverManager;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 public class CucumberHooks extends BaseTest {
 
     @Before
     public void setup() {
-        WebDriverManager.chromedriver().setup();
-        driver = new ChromeDriver();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--headless"); // Menjalankan tanpa GUI (dibutuhkan untuk GitHub Actions)
+        options.addArguments("--no-sandbox"); // Mencegah sandbox error
+        options.addArguments("--disable-dev-shm-usage"); // Menghindari error resource di GitHub Actions
+        options.addArguments("--user-data-dir=/tmp/chrome-user-data"); // Mengatasi error session di beberapa environment
+
+        driver = new ChromeDriver(options);
+        driver.manage().window().maximize();
     }
 
     @After
@@ -22,4 +27,5 @@ public class CucumberHooks extends BaseTest {
         }
     }
 }
+
 
