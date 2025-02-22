425cc62e2d50c6e27301b4406a2d1bd8d9f21446
Angel <angelaindahpuspita@gmail.com>
2025-02-22 10:00:13 +0800

    rev

--- src/test/java/com/angel/stepdef/CucumberHooks.java
+++ src/test/java/com/angel/stepdef/CucumberHooks.java
@@ -15,10 +15,27 @@ public class CucumberHooks extends BaseTest {
         driver = new ChromeDriver();
     }
 
-    @After
-    public void tearDown() {
-        if (driver != null) {
-            driver.quit();
+    package com.angel.stepdef;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import io.cucumber.java.Before;
+import io.cucumber.java.After;
+
+    public class CucumberHooks {
+        public static WebDriver driver;
+
+        @Before
+        public void setup() {
+            WebDriverManager.chromedriver().setup();
+            driver = new ChromeDriver();
+        }
+
+        @After
+        public void tearDown() {
+            if (driver != null) {
+                driver.quit();
+            }
         }
     }
-
