6cadd1bd71e4159eea75b91c0d7688498603fae2
Rian Febriansyah <rianfebriansyah22@gmail.com>
2025-02-25 20:45:50 +0700

    add api test

---
+++ src/test/java/com/angel/api/CucumberHooks.java
@@ -0,0 +1,18 @@
+package com.angel.api;
+
+import com.angel.api.helper.Endpoint;
+import io.cucumber.java.*;
+
+import java.util.Objects;
+
+public class   CucumberHooks {@BeforeAll
+public static void setUp(){
+    System.out.println("Before all test");
+    new Endpoint(); //run api in the beginning testing
+}
+
+    @AfterAll
+    public static void tearDown(){
+        System.out.println("After all test");
+    }
+}
